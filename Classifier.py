5f8470e0793519a64265c3197898720886c29151
TheEliasBe <elias.berger@tum.de>
2020-09-21 22:53:18 +0200

    Display FP, FN

--- Classifier.py
+++ Classifier.py
@@ -60,36 +60,56 @@ def resize_image():
 
         image = color.rgb2gray(image)
         image_rescaled = transform.resize(image, (avg_height, avg_width))
+        image_rescaled = transform.rescale(image_rescaled, 0.1)
         io.imsave(fname="media/resized/"+img_file[0], arr=image_rescaled)
         i += 1
         if i == 10:
             pass
 
+
 # read resized and greyed images an np-arrays
 pixels = []
 for img_file in files:
     img = io.imread("media/resized/"+img_file[0])
-    img = transform.rescale(img, 0.1)
     img = img.flatten()
     pixels.append(img)
 
-pixels = np.array(pixels, dtype=np.float)
 
+pixels = np.array(pixels, dtype=np.float)
 files = np.concatenate((files, pixels), axis=1)
 
-# remove file name, not necessary
-files = files[:,1:]
+# convert to data frame
+df = pd.DataFrame(files)
 
-# set dtype to float64
-files = files.astype('float64')
+# assign dtypes
+df[0] = df[0].astype('object')
+df.iloc[:,1:] = df.iloc[:,1:].astype('float')
 
 # split data into test and train
-X_train, X_test, y_train, y_test = train_test_split(files[:,2:16379], files[:,0], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,2:], df[1], test_size=0.2, random_state=42)
+y_train = np.asarray(y_train,dtype=np.float64)
+y_test = np.asarray(y_test,dtype=np.float64)
 
-print(X_train)
+# create classifier
 sgd_clf = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
 sgd_clf.fit(X_train, y_train)
 y_pred = sgd_clf.predict(X_test)
-print(y_pred)
 print(accuracy_score(y_test, y_pred))
 
+# find & display all misclassified instances
+a = np.hstack((np.c_[y_pred], np.c_[y_test], X_test))
+print(a[:,0:2])
+for line in a:
+    if line[0] == 0 and line[1] == 1:
+        # classified as non-chat but actually chat
+        i = np.reshape(line[2:], (159,103))
+        i = i.astype('float')
+        io.imshow(arr=i)
+        io.show()
+
+    if line[0] == 1 and line[1] == 0:
+        # classified as chat but actually non-chat
+        i = np.reshape(line[2:], (159,103))
+        i = i.astype('float')
+        io.imshow(arr=i)
+        io.show()

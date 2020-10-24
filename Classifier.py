fc7e10bb2864d50544bac0ade22b9dc915dde54a
TheEliasBe <elias.berger@tum.de>
2020-10-24 23:53:01 +0200

    Added contour detection

--- Classifier.py
+++ Classifier.py
@@ -26,10 +26,7 @@ non_chat_files = np.transpose(np.array([other_file_names]))
 non_chat_labels = np.transpose(np.array([[0.0]*len(non_chat_files)]))
 non_chat_files = np.concatenate((non_chat_files, non_chat_labels), axis=1)
 
-#print("Number of samples", len(chat_files)+len(non_chat_files), sep="\n")
-#print("Ratio Class 'Chat' of all samples", len(chat_files)/(len(chat_files)+len(non_chat_files)), sep="\n")
-
-# width, height constant as an average of ca. 400 images
+# width, height constant as an average of ax. 400 images
 WIDTH = 1024
 HEIGHT = 1607
 
@@ -99,24 +96,14 @@ def save_hog(width, height):
         fd, hog_image = hog(image, orientations=8, pixels_per_cell=(4, 4),cells_per_block = (1, 1), visualize = True, multichannel = False)
         np.save("media/hog/"+img_file[0], hog_image)
 
-def train_model(use_hog=False):
+def generate_train_test_data():
     global files
     # read resized and greyed images an np-arrays
     pixels = []
-    if use_hog:
-        files = os.listdir("media/hog")
-        for img_file in files:
-            print("Reading HOG", img_file)
-            img = np.load("media/hog/"+img_file)
-            img = img.flatten()
-            pixels.append(img)
-    else:
-        for img_file in files:
-            img = io.imread("media/resized/"+img_file[0])
-            img = img.flatten()
-            pixels.append(img)
-
-
+    for img_file in files:
+        img = io.imread("media/resized/" + img_file[0])
+        img = img.flatten()
+        pixels.append(img)
     pixels = np.array(pixels, dtype=np.float)
     files = np.concatenate((files, pixels), axis=1)
 
@@ -125,10 +112,10 @@ def train_model(use_hog=False):
 
     # assign dtypes
     df[0] = df[0].astype('object')
-    df.iloc[:,1:] = df.iloc[:,1:].astype('float')
+    df.iloc[:, 1:] = df.iloc[:, 1:].astype('float')
 
     # set X and y
-    X = df.iloc[:,2:].astype('float')
+    X = df.iloc[:, 2:].astype('float')
     y = df[1].astype('int')
 
     # random over sample the non-chat files
@@ -145,13 +132,15 @@ def train_model(use_hog=False):
 
     # split data into test and train
     X_train, X_test, y_train, y_test = train_test_split(X_over, y_over, test_size=0.2, random_state=42)
-    y_train = np.asarray(y_train,dtype=np.float64)
-    y_test = np.asarray(y_test,dtype=np.float64)
+    y_train = np.asarray(y_train, dtype=np.float64)
+    y_test = np.asarray(y_test, dtype=np.float64)
 
+    return X_train, y_train, X_test, y_test
+
+def train_model(X_train, y_train, X_test, y_test, store_model=False):
     # create classifier
     clf = MLPClassifier(hidden_layer_sizes=(100, 100, 10), max_iter=1000, learning_rate='adaptive')
-    with open("pickle_model_90_1.pkl", 'rb') as file:
-        clf2 = pickle.load(file)
+
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print("Accuracy", accuracy_score(y_test, y_pred), sep="\n")
@@ -159,28 +148,25 @@ def train_model(use_hog=False):
     df_cm = pd.DataFrame(cm, range(2), range(2))
     sn.heatmap(df_cm, annot=True)
     plt.show()
-    pkl_filename = "pickle_model_90_1.pkl"
-    with open(pkl_filename, 'wb') as file:
-       pickle.dump(clf, file)
+    if store_model:
+        pkl_filename = "pickle_model_90_1.pkl"
+        with open(pkl_filename, 'wb') as file:
+           pickle.dump(clf, file)
 
-    # find & display all misclassified instances
-    a = np.hstack((np.c_[y_pred], np.c_[y_test], X_test))
+def test_pickel():
+    global files
+    pixels = []
+    X_train, y_train, X_test, y_test = generate_train_test_data()
+    # Load from file
+    with open("pickle_model_90_1.pkl", 'rb') as file:
+        pickle_model = pickle.load(file)
+        y_pred = pickle_model.predict(X_test)
+        cm = confusion_matrix(y_test, y_pred)
+        print("Accuracy", accuracy_score(y_test, y_pred), sep="\n")
+        df_cm = pd.DataFrame(cm, range(2), range(2))
+        sn.heatmap(df_cm, annot=True)
+        plt.show()
 
-
-    # for line in a:
-    #     if line[0] == 0 and line[1] == 1:
-    #         # classified as non-chat but actually chat
-    #         i = np.reshape(line[2:], (159,103))
-    #         i = i.astype('float')
-    #         io.imshow(arr=i)
-    #         io.show()
-    #
-    #     if line[0] == 1 and line[1] == 0:
-    #         # classified as chat but actually non-chat
-    #         i = np.reshape(line[2:], (159,103))
-    #         i = i.astype('float')
-    #         io.imshow(arr=i)
-    #         io.show()
 
 def classify(img_arr, model_file_name):
     for index in range(len(img_arr)):
@@ -213,7 +199,7 @@ i4 = io.imread("media/resized/2s7q2tbdd1o51.png")
 i5 = io.imread("media/downscaled.jpg")
 i6 = io.imread("media/hg.png")
 
-train_model()
+test_pickel()
 # class_id, prob = classify([i6], "pickle_model_90_1.pkl")
 #
 # if class_id[0] == 0:
--- Localizer.py
+++ Localizer.py
@@ -1,12 +1,20 @@
 from pytesseract import Output
 import pytesseract
-import argparse
 import cv2
 import pandas as pd
+from TextBubbleDetector import Detector, is_in_bounding_box
+import imutils
+from skimage import io
+import numpy as np
 
-images = cv2.imread("media/convo/0kby0qwor6h21.jpg")
+f = ["media/convo/0tkirgfqmcc31.png", "media/convo/0q3roi8vo3j41.jpg", "media/convo/0kby0qwor6h21.jpg"]
+images = cv2.imread(f[0])
+
+resized = imutils.resize(images, width=300)
+ratio = images.shape[0] / float(resized.shape[0])
+
 rgb = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
-results = pytesseract.image_to_data(rgb, output_type=Output.DICT, lang='eng')
+results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
 text = pytesseract.image_to_boxes(rgb)
 
 # number of pixels +- still considered aline of text
@@ -17,7 +25,8 @@ line_break_threshold = 66
 
 # variables
 lines = []
-lines_df = pd.DataFrame(columns=['text', 'x', 'y', 'width'])
+lines_df = pd.DataFrame(columns=['text', 'x', 'y', 'width', 'height'])
+word_df = pd.DataFrame(columns=['text', 'x', 'y', 'width', 'height'])
 l = []
 curr_y = 0
 curr_x = 0
@@ -38,15 +47,16 @@ for i in range(0, len(results["text"])):
 	conf = int(results["conf"][i])
 
 
-	if conf > 90 and text != " ":
+	if conf > 10 and text != " ":
 		# print(text, "Y", y, "ΔY", abs(curr_y - y))
 		total_width += w
-
+		cv2.rectangle(rgb, (x,y), (x+w, y+h), (0,0,255),2)
+		word_df = word_df.append({'text': text, 'x': x, 'y': y, 'width': w, 'height': h}, ignore_index=True)
 		if abs(curr_y - y) <= line_threshold or curr_y == 0:
 			l.append(text)
 			x_list.append(x)
 		else:
-			lines_df = lines_df.append({'text': l.copy(), 'x': min(x_list), 'y': curr_y, 'width': total_width}, ignore_index=True)
+			lines_df = lines_df.append({'text': l.copy(), 'x': min(x_list), 'y': curr_y, 'width': total_width, 'height': h}, ignore_index=True)
 			x_list = []
 			x_list.append(x)
 			# print("Line Finished", l)
@@ -56,31 +66,51 @@ for i in range(0, len(results["text"])):
 			l.append(text)
 		curr_y = y
 
-# merge lines into text blocks
-curr_y = 0
-x = 0
-width = 0
-text_blocks = []
-text_blocks_df = pd.DataFrame(columns=['text', 'x', 'y', 'width'])
-string = []
+print("word_df")
+print(word_df)
 
-for index, line in lines_df.iterrows():
-	y = line['y']
-	x = line['x']
-	width = line['width']
-	if abs(curr_y - y) <= line_break_threshold or curr_y == 0:
-		x_list.append(x)
-		for word in line['text']:
-			string.append(word)
-	else:
-		text_blocks_df = text_blocks_df.append({'text': ' '.join(string), 'x': min(x_list), 'y': curr_y, 'width': line['width']}, ignore_index=True)
-		string = []
-		for word in line['text']:
-			string.append(word)
-		x_list = []
-		x_list.append(x)
-	curr_y = y
+# find locations of text bubbles
 
-text_blocks_df = text_blocks_df.append({'text': ' '.join(string), 'x': min(x_list), 'y': curr_y, 'width': width}, ignore_index=True)
+# grayscale the image, blur it and apply threshold to emphasize egdes
+gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+blurred = cv2.GaussianBlur(gray, (15, 15), 0)
+thresh = cv2.threshold(blurred, 247, 255, cv2.THRESH_BINARY)[1]
 
-print(text_blocks_df)
+# find contours in the thresholded image and initialize the
+cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+bounding_boxes = pd.DataFrame(columns=['x', 'y', 'w', 'h', 'text', 'partner'])
+bounding_boxes['text'] = bounding_boxes['text'].astype('string')
+
+# find bounding rectangle of each contour
+for c in cnts:
+	x, y, w, h = cv2.boundingRect(c)
+	bounding_boxes = bounding_boxes.append({'x': x, 'y': y, 'w': w, 'h': h, 'text': ''}, ignore_index=True)
+
+
+# assign each text box to a bounding box
+left_most = bounding_boxes['x']
+right_most = bounding_boxes['x'] + bounding_boxes['w']
+
+# remove bounding box covering the entire image
+h, w, _ = np.shape(rgb)
+bounding_boxes = bounding_boxes[bounding_boxes['w'] != w]
+
+
+for i2, b in bounding_boxes.iterrows():
+	for i1, t in word_df.iterrows():
+		box_dim = {'x': int(b['x']), 'y': int(b['y']), 'w': int(b['w']), 'h': int(b['h'])}
+		text_dim = {'x': t['x'], 'y': t['y'], 'w': t['width'], 'h': t['height']}
+		cv2.rectangle(rgb, (box_dim['x'], box_dim['y']), (box_dim['x']+box_dim['w'], box_dim['y']+box_dim['h']), (255,0,0), 2)
+		cv2.rectangle(rgb, (text_dim['x'], text_dim['y']), (text_dim['x'] + text_dim['w'], text_dim['y'] + text_dim['h']), (0, 255, 0), 2)
+		if is_in_bounding_box(box_dim, text_dim):
+			#print(box_dim)
+			#print(text_dim)
+			bounding_boxes.at[i2, 'text'] = str(bounding_boxes.at[i2, 'text']) + " " +str(t['text'])
+print(bounding_boxes)
+
+# assign bounding boxes to chat sides
+for i, b in bounding_boxes.iterrows():
+	...
+
+cv2.imwrite('image.jpg', rgb)
